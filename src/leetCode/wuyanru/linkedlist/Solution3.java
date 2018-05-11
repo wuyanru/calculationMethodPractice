431842d853de61e9ccbf892cbc19649ee56e58b1
wuyanru <15535637452@qq.com>
2018-05-11 15:00:45 +0800

    11

---
+++ src/leetCode/wuyanru/linkedlist/Solution3.java
@@ -0,0 +1,33 @@
+package leetCode.wuyanru.linkedlist;
+
+import java.util.Stack;
+
+import com.wuyanru.tree.Solution47;
+import com.wuyanru.tree.TreeNode;
+
+
+/**
+ * 
+ Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+    
+ */
+class Point {
+	      int x;
+	      int y;
+	      Point() { x = 0; y = 0; }
+	      Point(int a, int b) { x = a; y = b; }
+	  }
+public class Solution3 {
+public int maxPoints(Point[] points) {
+	
+	
+	return 0;
+    }
+	
+	public static void main(String[] args) {
+		String str[]={"4","3","-"};
+		Solution3 s=new Solution3();
+
+	}
+	
+}
